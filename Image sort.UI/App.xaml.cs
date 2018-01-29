c355b8bf851ce8ced82a833d6173f8df94133bec
Luca Auer <Lolle@LUCA-TABLET>
2018-01-29 20:10:27 +0100

    Fully implement the installer to run at startup of the app.

--- Image sort.UI/App.xaml.cs
+++ Image sort.UI/App.xaml.cs
@@ -13,5 +13,18 @@ namespace Image_sort.UI
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Overridden so that the updater can check for updates before startup
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Run the Updater before starting the app
+            System.Diagnostics.Process.Start(AppDomain.CurrentDomain.BaseDirectory 
+                + @"\Image sort.Update.exe").WaitForExit();
+
+            // Continue normally
+            base.OnStartup(e);
+        }
     }
 }
