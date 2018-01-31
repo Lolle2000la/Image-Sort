7c2d58ee01fddd9ccebea1a17a5b1bf9e9f33ee6
Luca Auer <Lolle@LUCA-TABLET>
2018-01-31 23:19:00 +0100

    Fixed some issues

--- Image sort.Update/Properties/Resources.Designer.cs
+++ Image sort.Update/Properties/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace Image_sort.Update.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.3.0.
+        ///   Looks up a localized string similar to 1.3.1.
         /// </summary>
         internal static string version {
             get {
