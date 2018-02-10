488fffe5dd18113e997f299ca72c5bd55b12d969
Luca Auer <Lolle@LUCA-TABLET>
2018-02-10 01:09:30 +0100

    Pushed up version number to v1.6.1

--- Image sort.Update/Properties/Resources.Designer.cs
+++ Image sort.Update/Properties/Resources.Designer.cs
@@ -70,7 +70,7 @@ namespace Image_sort.Update.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.6.0.
+        ///   Looks up a localized string similar to 1.6.1.
         /// </summary>
         internal static string version {
             get {
