86098b8701dcfda324ed597fd7010776eaf28de8
Luca Auer <lolle2000.la@gmail.com>
2018-10-20 16:19:55 +0200

    Laid the base for updating the accent color on Windows Accent Color changes

---
+++ Image sort.UI/Classes/MessageFilters/ColorizationMessageFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace Image_sort.UI.Classes.MessageFilters
+{
+    class ColorizationMessageFilter : MessageFilter<Color>
+    {
+
+        /// <summary>
+        /// Message-code for changing colorization.
+        /// </summary>
+        private const uint WM_DWMCOLORIZATIONCOLORCHANGED = 0x0320;
+
+        protected override void OnThreadFilterMessageRaised(ref MSG msg, ref bool handled)
+        {
+            if (msg.message == WM_DWMCOLORIZATIONCOLORCHANGED)
+            {
+                int wParam = msg.wParam.ToInt32();
+                RaiseActionWith(Color.FromArgb(255,
+                    (byte) (wParam >> 16),
+                    (byte) (wParam >> 8),
+                    (byte) wParam));
+                handled = true;
+            }
+
+            handled = false;
+            
+            base.OnThreadFilterMessageRaised(ref msg, ref handled);
+        }
+    }
+}
---
+++ Image sort.UI/Classes/MessageFilters/MessageFilter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace Image_sort.UI.Classes.MessageFilters
+{
+    class MessageFilter<TMessageParam>
+    {
+        private Action<TMessageParam> messageHandler = null;
+
+        /// <summary>
+        /// Constructs a new instance of the message filter.
+        /// </summary>
+        public MessageFilter()
+        {
+            ComponentDispatcher.ThreadFilterMessage += OnThreadFilterMessageRaised;
+        }
+
+        /// <summary>
+        /// Constructs a new instance of the message filter.
+        /// </summary>
+        /// <param name="messageHandler">
+        /// Handler called, when the specified message is raised.
+        /// </param>
+        public MessageFilter(Action<TMessageParam> messageHandler)
+        {
+            this.messageHandler = messageHandler;
+
+            ComponentDispatcher.ThreadFilterMessage += OnThreadFilterMessageRaised;
+        }
+
+        /// <summary>
+        /// Processes the messages, handles them and calls the <see cref="messageHandler"/>.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="handled"></param>
+        protected virtual void OnThreadFilterMessageRaised(ref MSG msg, ref bool handled)
+        {
+            LogMessage(msg, handled);
+        }
+
+        [Conditional("DEBUG")]
+        private void LogMessage(MSG msg, bool handled)
+        {
+            Debug.WriteLine($"Message {msg.message} raised with lParam={msg.lParam} and wParam={msg.wParam}; Handled={handled}");
+        }
+        
+        /// <summary>
+        /// Raises the action with the message param, when given.
+        /// </summary>
+        /// <param name="messageParam">The parameter, that the action should be raised with.</param>
+        protected void RaiseActionWith(TMessageParam messageParam)
+        {
+            messageHandler?.Invoke(messageParam);
+        }
+    }
+}
