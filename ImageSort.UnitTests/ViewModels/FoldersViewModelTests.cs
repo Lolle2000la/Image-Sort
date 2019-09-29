0765b113d97b018810934711f4d9210ab8fb8d73
Luca Auer <lolle2000.la@gmail.com>
2019-09-29 18:41:03 +0200

    Add tests for FolderViewModel and add stuff for pinnable folders

---
+++ ImageSort.UnitTests/ViewModels/FoldersViewModelTests.cs
@@ -0,0 +1,76 @@
+﻿using ImageSort.FileSystem;
+using ImageSort.ViewModels;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using Xunit;
+
+namespace ImageSort.UnitTests.ViewModels
+{
+    public class FoldersViewModelTests
+    {
+        private const string MockPath = @"C:\SomePath\";
+
+        private FolderTreeItemViewModel CreateMock(string path)
+        {
+            var fsMock = new Mock<IFileSystem>();
+
+            return new FolderTreeItemViewModel(fsMock.Object)
+            {
+                Path = path
+            };
+        }
+
+        [Fact(DisplayName = "The currently loaded folder can be changed.")]
+        public void CurrentFolderCanBeChanged()
+        {
+            var foldersVM = new FoldersViewModel();
+
+            foldersVM.CurrentFolder = CreateMock(MockPath);
+
+            Assert.Equal(MockPath, foldersVM.CurrentFolder.Path);
+        }
+
+        [Fact(DisplayName = "Can prompt user to pin folders.")]
+        public void CanPinFolders()
+        {
+            const string mockPathToPin = @"C:\SomeOtherPath\";
+
+            var foldersVM = new FoldersViewModel
+            {
+                CurrentFolder = CreateMock(MockPath)
+            };
+
+            foldersVM.SelectFolder.RegisterHandler(interaction =>
+            {
+                interaction.SetOutput(mockPathToPin);
+            });
+
+            foldersVM.Pin.Execute().ToTask().Wait();
+
+            Assert.NotNull(foldersVM.PinnedFolders);
+
+            Assert.Contains(mockPathToPin, foldersVM.PinnedFolders
+                .Select(f => f.Path));
+        }
+
+        [Fact(DisplayName = "Handles user canceling gracefully.")]
+        public void HandlesCancelingGracefully()
+        {
+            const string mockPathToPin = @"C:\SomeOtherPath\";
+
+            var foldersVM = new FoldersViewModel
+            {
+                CurrentFolder = CreateMock(MockPath)
+            };
+
+            foldersVM.Pin.Execute();
+
+            Assert.DoesNotContain(mockPathToPin, foldersVM.PinnedFolders
+                .Select(f => f.Path));
+        }
+    }
+}
--- ImageSort/ViewModels/FoldersViewModel.cs
+++ ImageSort/ViewModels/FoldersViewModel.cs
@@ -1,6 +1,11 @@
-﻿using ReactiveUI;
+﻿using ImageSort.FileSystem;
+using ReactiveUI;
+using Splat;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Reactive;
+using System.Reactive.Linq;
 
 namespace ImageSort.ViewModels
 {
@@ -12,13 +17,7 @@ namespace ImageSort.ViewModels
             get => _currentFolder;
             set => this.RaiseAndSetIfChanged(ref _currentFolder, value);
         }
-
-        private IEnumerable<FolderTreeItemViewModel> _pinnedFolders;
-        public IEnumerable<FolderTreeItemViewModel> PinnedFolders
-        {
-            get => _pinnedFolders;
-            set => this.RaiseAndSetIfChanged(ref _pinnedFolders, value);
-        }
+        public ObservableCollection<FolderTreeItemViewModel> PinnedFolders { get; } = new ObservableCollection<FolderTreeItemViewModel>();
 
         private FolderTreeItemViewModel _selected;
         public FolderTreeItemViewModel Selected
@@ -27,8 +26,25 @@ namespace ImageSort.ViewModels
             set => this.RaiseAndSetIfChanged(ref _selected, value);
         }
 
-        public ReactiveCommand<string, Unit> Pin { get; }
+        /// <summary>
+        /// Should prompt the user to select a folder.
+        /// </summary>
+        public Interaction<Unit, string> SelectFolder { get; } 
+            = new Interaction<Unit, string>();
+
+        public ReactiveCommand<Unit, Unit> Pin { get; }
         public ReactiveCommand<Unit, Unit> PinSelected { get; }
         public ReactiveCommand<Unit, Unit> UnpinSelected { get; }
+
+        public FoldersViewModel()
+        {
+            Pin = ReactiveCommand.Create(() => 
+            {
+                SelectFolder.Handle(Unit.Default)
+                    .Subscribe(folder => PinnedFolders.Add(
+                        new FolderTreeItemViewModel(
+                            Locator.Current.GetService<IFileSystem>())));
+            });
+        }
     }
 }
