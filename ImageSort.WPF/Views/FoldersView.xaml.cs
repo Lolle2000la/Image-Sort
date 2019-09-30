61d62263793bbd610c217559814b057fc1250e5b
Luca Auer <lolle2000.la@gmail.com>
2019-09-30 21:38:59 +0200

    Add FoldersView, the view for FoldersViewModel

---
+++ ImageSort.WPF/Views/FoldersView.xaml.cs
@@ -0,0 +1,59 @@
+﻿using ImageSort.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageSort.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for FoldersView.xaml
+    /// </summary>
+    public partial class FoldersView : ReactiveUserControl<FoldersViewModel>
+    {
+        public FoldersView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                ViewModel.SelectFolder.RegisterHandler(ic =>
+                {
+                    var folderBrowser = new FolderBrowserDialog() 
+                    { 
+                        ShowNewFolderButton = true
+                    };
+
+                    if (folderBrowser.ShowDialog() == DialogResult.OK)
+                        ic.SetOutput(folderBrowser.SelectedPath);
+                });
+
+                this.Bind(ViewModel,
+                    vm => vm.Selected,
+                    view => view.Folders.SelectedItem)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel,
+                    vm => vm.AllFoldersTracked,
+                    view => view.Folders.ItemsSource)
+                    .DisposeWith(disposableRegistration);
+
+                this.BindCommand(ViewModel,
+                    vm => vm.Pin,
+                    view => view.Pin)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
