a264d4792ee8e38e4cb9e8622b55fd17551de678
Luca Auer <lolle2000.la@gmail.com>
2020-02-03 21:52:02 +0100

    Add property for the current path

--- ImageSort/ViewModels/ImagesViewModel.cs
+++ ImageSort/ViewModels/ImagesViewModel.cs
@@ -4,5 +4,11 @@ namespace ImageSort.ViewModels
 {
     public class ImagesViewModel : ReactiveObject
     {
+        private string _currentPath;
+        public string CurrentFolder
+        {
+            get => _currentPath;
+            set => this.RaiseAndSetIfChanged(ref _currentPath, value);
+        }
     }
 }
