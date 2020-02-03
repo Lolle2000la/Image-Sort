4b69a9e893454cd64483f738572bd8f7d8c25471
Luca Auer <lolle2000.la@gmail.com>
2020-02-03 20:35:09 +0100

    Remove unnecessary test

--- ImageSort.UnitTests/ViewModels/FolderTreeItemViewModelTests.cs
+++ ImageSort.UnitTests/ViewModels/FolderTreeItemViewModelTests.cs
@@ -32,12 +32,10 @@ namespace ImageSort.UnitTests.ViewModels
 
             fsMock.Setup(fs => fs.GetSubFolders(path)).Returns(resultingPaths);
 
-            var folderTreeItem = new FolderTreeItemViewModel(fsMock.Object) 
+            var folderTreeItem = new FolderTreeItemViewModel(fsMock.Object, RxApp.MainThreadScheduler) 
             { 
                 Path = path
             };
-
-            folderTreeItem.IsExpanded = true;
 
             var obtainedPaths = folderTreeItem.Children;
 
@@ -59,42 +57,6 @@ namespace ImageSort.UnitTests.ViewModels
             {
                 Path = pathToUnauthorisedFolder
             };
-
-            folderTreeItem.IsExpanded = true;
-
-            fsMock.Verify(fs => fs.GetSubFolders(pathToUnauthorisedFolder));
-
-            Assert.Null(folderTreeItem.Children);
-        }
-
-        [Fact(DisplayName = "Only allows expanding of the current folder (IsExpandable) if that is possible.")]
-        public void OnlyAllowsExpansionWhenPossible()
-        {
-            const string pathToEmptyFolder = @"C:\EmptyFolder";
-            const string pathToNonEmptyFolder = @"C:\NonEmptyFolder";
-            const string pathToUnauthorisedFolder = @"C:\UnauthorizedFolder";
-
-            var fsMock = new Mock<IFileSystem>();
-
-            fsMock.Setup(fs => fs.IsFolderEmpty(pathToNonEmptyFolder)).Returns(false);
-            fsMock.Setup(fs => fs.IsFolderEmpty(pathToEmptyFolder)).Returns(true);
-            fsMock.Setup(fs => fs.IsFolderEmpty(pathToUnauthorisedFolder)).Throws(new UnauthorizedAccessException());
-
-            var folderTreeItem = new FolderTreeItemViewModel(fsMock.Object, RxApp.MainThreadScheduler)
-            {
-                Path = pathToNonEmptyFolder
-            };
-
-            Assert.True(folderTreeItem.IsExpandable);
-            fsMock.Verify(fs => fs.IsFolderEmpty(pathToNonEmptyFolder));
-
-            folderTreeItem.Path = pathToEmptyFolder;
-            Assert.False(folderTreeItem.IsExpandable);
-            fsMock.Verify(fs => fs.IsFolderEmpty(pathToEmptyFolder));
-
-            folderTreeItem.Path = pathToUnauthorisedFolder;
-            Assert.False(folderTreeItem.IsExpandable);
-            fsMock.Verify(fs => fs.IsFolderEmpty(pathToUnauthorisedFolder));
         }
     }
 }
