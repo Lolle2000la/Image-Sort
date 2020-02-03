a3258358c8bd4c7db679d70d877daaf5a763c969
Luca Auer <lolle2000.la@gmail.com>
2020-02-03 21:32:43 +0100

    Prevent opening of folders that do not make sense

--- ImageSort/ViewModels/MainViewModel.cs
+++ ImageSort/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 
 namespace ImageSort.ViewModels
@@ -22,10 +23,16 @@ namespace ImageSort.ViewModels
 
         public MainViewModel()
         {
+            var canOpenCurrentlySelectedFolder = this.WhenAnyValue(x => x.Folders)
+                .Where(f => f != null)
+                .SelectMany(f => f.WhenAnyValue(x => x.Selected, x => x.CurrentFolder, (s, c) => new { Selected = s, CurrentFolder = c }))
+                .Where(f => f != null)
+                .Select(f => f.Selected != null && f.Selected != f.CurrentFolder);
+
             OpenCurrentlySelectedFolder = ReactiveCommand.Create(() =>
             {
                 Folders.CurrentFolder = Folders.Selected;
-            });
+            }, canOpenCurrentlySelectedFolder);
         }
     }
 }
