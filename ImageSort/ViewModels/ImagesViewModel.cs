719e2ab562774272f0e490999d1b1f669dacdff0
Luca Auer <lolle2000.la@gmail.com>
2020-02-04 18:20:31 +0100

    Add ability to list images in a folder

--- ImageSort/ViewModels/ImagesViewModel.cs
+++ ImageSort/ViewModels/ImagesViewModel.cs
@@ -1,4 +1,12 @@
-﻿using ReactiveUI;
+﻿using ImageSort.FileSystem;
+using ImageSort.Helpers;
+using ReactiveUI;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
 
 namespace ImageSort.ViewModels
 {
@@ -9,6 +17,28 @@ namespace ImageSort.ViewModels
         {
             get => _currentPath;
             set => this.RaiseAndSetIfChanged(ref _currentPath, value);
+        }
+
+        private readonly ObservableAsPropertyHelper<IEnumerable<string>> _images;
+        public IEnumerable<string> Images => _images.Value;
+
+        private string _currentImage;
+        public string CurrentImage
+        {
+            get => _currentImage;
+            set => this.RaiseAndSetIfChanged(ref _currentImage, value);
+        }
+
+        public ImagesViewModel(IFileSystem fileSystem = null)
+        {
+            fileSystem = fileSystem ?? Locator.Current.GetService<IFileSystem>();
+
+            _images = this.WhenAnyValue(x => x.CurrentFolder)
+                .Select(f => fileSystem.GetFiles(f)
+                                      .Where(s => s.EndsWithAny(
+                                          StringComparison.OrdinalIgnoreCase,
+                                          ".png", ".jpg", ".gif", ".bmp", ".tiff", ".tif", ".ico")))
+                .ToProperty(this, x => x.Images);
         }
     }
 }
