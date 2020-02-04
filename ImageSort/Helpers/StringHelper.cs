46b6413c35a5c706bb20e32211ab147e64ba5bec
Luca Auer <lolle2000.la@gmail.com>
2020-02-04 18:16:57 +0100

    Simple helper checking that a string ends with any of the files

---
+++ ImageSort/Helpers/StringHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSort.Helpers
+{
+    static class StringHelper
+    {
+        public static bool EndsWithAny(
+            this string @string,
+            StringComparison comparisonType,
+            params string[] atEnd)
+        {
+            var endsWithIt = false;
+
+            foreach(var end in atEnd)
+            {
+                if (@string.EndsWith(end, comparisonType)) endsWithIt = true;
+            }
+
+            return endsWithIt;
+        }
+    }
+}
