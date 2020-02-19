57ae0867990f1beeb8796ee840603dc75e16842a
Luca Auer <lolle2000.la@gmail.com>
2020-02-19 18:00:12 +0100

    Add a view for viewing the images in the current folder

---
+++ ImageSort.WPF/Views/ImagesView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ImageSort.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Net.Cache;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageSort.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ImagesView.xaml
+    /// </summary>
+    public partial class ImagesView : ReactiveUserControl<ImagesViewModel>
+    {
+        public ImagesView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.OneWayBind(ViewModel,
+                    vm => vm.SelectedImage,
+                    view => view.SelectedImage.Source,
+                    p => new BitmapImage(new Uri(p)))
+                    .DisposeWith(disposableRegistration);
+
+                //this.OneWayBind(ViewModel,
+                //    vm => vm.Images,
+                //    view => view.Images.ItemsSource)
+                //    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
