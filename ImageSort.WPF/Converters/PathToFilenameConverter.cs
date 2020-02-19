63dc6e0ce03f4a67cc5c979f2aee06d1922c50a4
Luca Auer <lolle2000.la@gmail.com>
2020-02-19 20:28:27 +0100

    Add a converter for turning paths into filenames

---
+++ ImageSort.WPF/Converters/PathToFilenameConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+
+namespace ImageSort.WPF.Converters
+{
+    class PathToFilenameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            if (value is string path) return Path.GetFileName(path);
+
+            throw new ArgumentException("Value should be a string.", nameof(value));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
