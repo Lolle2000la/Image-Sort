2c2c736139cd171ea236f8a4f780585b62c5b406
Luca Auer <lolle2000.la@gmail.com>
2020-02-20 13:53:43 +0100

    Add test validating moveactions behavior

---
+++ ImageSort.UnitTests/Actions/MoveActionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImageSort.Actions;
+using ImageSort.FileSystem;
+using Moq;
+using Xunit;
+
+namespace ImageSort.UnitTests.Actions
+{
+    public class MoveActionTests
+    {
+        [Fact(DisplayName = "File gets moved correctly.")]
+        public void FileGetsMovedCorrectly()
+        {
+            const string oldPath = @"C:\SomeFile.png";
+            const string newFolder = @"C:\SomeOtherFolder\SomeOtherFolderAsWell\";
+            const string newPath = newFolder + "SomeFile.png";
+
+            var fsMock = new Mock<IFileSystem>();
+
+            fsMock.Setup(fs => fs.Move(oldPath, newPath)).Verifiable();
+            fsMock.Setup(fs => fs.Move(newPath, oldPath)).Verifiable();
+            fsMock.Setup(fs => fs.FileExists(oldPath)).Returns(true);
+            fsMock.Setup(fs => fs.DirectoryExists(newFolder)).Returns(true);
+
+            var moveAction = new MoveAction(oldPath, newFolder, fsMock.Object);
+
+            fsMock.Verify(fs => fs.FileExists(oldPath));
+            fsMock.Verify(fs => fs.DirectoryExists(newFolder));
+
+            moveAction.Act();
+
+            fsMock.Verify(fs => fs.Move(oldPath, newPath));
+
+            moveAction.Revert();
+
+            fsMock.Verify(fs => fs.Move(newPath, oldPath));
+        }
+    }
+}
