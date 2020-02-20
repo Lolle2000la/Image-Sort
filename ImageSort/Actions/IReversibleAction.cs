cb1dd358b38e8dfd40625631a38b2386bedaa6a7
Luca Auer <lolle2000.la@gmail.com>
2020-02-20 13:19:57 +0100

    Add the concept of an action that can be reverted

---
+++ ImageSort/Actions/IReversibleAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSort.Actions
+{
+    public interface IReversibleAction
+    {
+        void Act();
+        void Revert();
+    }
+}
