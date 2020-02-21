963d479f01098085985be52311ed2cbf972ea310
Luca Auer <lolle2000.la@gmail.com>
2020-02-21 20:45:20 +0100

    Clarify documentation

--- ImageSort/FileSystem/IRecycleBin.cs
+++ ImageSort/FileSystem/IRecycleBin.cs
@@ -12,6 +12,10 @@ namespace ImageSort.FileSystem
         /// <param name="path">The path to the file or folder that should be send to the recycle bin.</param>
         /// <param name="confirmationNeeded">Should ask for user confirmation before sending to recycle bin.</param>
         /// <returns>The <see cref="IDisposable"/> will, when disposed, restore the file.</returns>
+        /// <remarks>
+        /// The <see cref="IDisposable"/> handle to the deleted file will throw a <see cref="FileRestorationNotPossibleException"/> 
+        /// when it cannot restore the file.
+        /// </remarks>
         IDisposable Send(string path, bool confirmationNeeded = false);
     }
 }
