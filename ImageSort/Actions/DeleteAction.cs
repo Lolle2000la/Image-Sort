85640f672eaff14d17064ed4d59d62a4a37cc3dc
Luca Auer <lolle2000.la@gmail.com>
2020-02-21 19:55:59 +0100

    Set members

--- ImageSort/Actions/DeleteAction.cs
+++ ImageSort/Actions/DeleteAction.cs
@@ -1,15 +1,27 @@
 ﻿using ImageSort.FileSystem;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ImageSort.Actions
 {
     public class DeleteAction : IReversibleAction
     {
-        public DeleteAction(string path, IRecycleBin fileSystem)
-        {
+        private readonly string oldPath;
+        private readonly IFileSystem fileSystem;
+        private readonly IRecycleBin recycleBin;
 
+        public DeleteAction(string path, IFileSystem fileSystem, IRecycleBin recycleBin)
+        {
+            if (path == null) throw new ArgumentNullException(nameof(path));
+            if (fileSystem == null) throw new ArgumentNullException(nameof(fileSystem));
+            if (recycleBin == null) throw new ArgumentNullException(nameof(recycleBin));
+            if (!fileSystem.FileExists(path)) throw new FileNotFoundException(null, path);
+
+            oldPath = path;
+            this.fileSystem = fileSystem;
+            this.recycleBin = recycleBin;
         }
 
         public void Act()
