4f45302472a1e8ab03e9a693a2d56b3a7e853622
Luca Auer <lolle2000.la@gmail.com>
2020-02-21 21:36:37 +0100

    Add tests for ActionsViewModel

---
+++ ImageSort.UnitTests/ViewModels/ActionsViewModelTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImageSort.ViewModels;
+using ImageSort.Actions;
+using Xunit;
+using Moq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace ImageSort.UnitTests.ViewModels
+{
+    public class ActionsViewModelTest
+    {
+        [Fact(DisplayName = "Executes an action, adds it to the history, allows it to be undone and allows it to be redone and makes the last un-/done visible.")]
+        public async Task WorksCorrectly()
+        {
+            const string actionDisplayName = "Test action display name";
+
+            var actionsVM = new ActionsViewModel();
+
+            var actionMock = new Mock<IReversibleAction>();
+
+            actionMock.Setup(a => a.Act()).Verifiable();
+            actionMock.Setup(a => a.Revert()).Verifiable();
+            actionMock.SetupGet(a => a.DisplayName).Returns(actionDisplayName);
+
+            await actionsVM.Execute.Execute(actionMock.Object);
+
+            Assert.Equal(actionDisplayName, actionsVM.LastDone);
+
+            await actionsVM.Undo.Execute();
+
+            Assert.Equal(actionDisplayName, actionsVM.LastUndone);
+            Assert.NotEqual(actionDisplayName, actionsVM.LastDone);
+
+            await actionsVM.Redo.Execute();
+
+            Assert.Equal(actionDisplayName, actionsVM.LastDone);
+            Assert.NotEqual(actionDisplayName, actionsVM.LastUndone);
+
+            actionMock.Verify(a => a.Act(), Times.Exactly(2));
+            actionMock.Verify(a => a.Revert(), Times.Once);
+        }
+    }
+}
