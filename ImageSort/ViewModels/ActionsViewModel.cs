e5e34242fb29e9b67f71f7f9afd8be1e3dfcfd7e
Luca Auer <lolle2000.la@gmail.com>
2020-02-21 21:18:36 +0100

    Outline api for ActionsViewModel

---
+++ ImageSort/ViewModels/ActionsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ImageSort.Actions;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Text;
+
+namespace ImageSort.ViewModels
+{
+    public class ActionsViewModel : ReactiveObject
+    {
+        private readonly Stack<IReversibleAction> done;
+        private readonly Stack<IReversibleAction> undone;
+
+        private readonly ObservableAsPropertyHelper<string> lastDone;
+        public string LastDone => lastDone.Value;
+
+        private readonly ObservableAsPropertyHelper<string> lastUndone;
+        public string LastUndone => lastUndone.Value;
+
+        public ReactiveCommand<Unit, Unit> Undo { get; }
+        public ReactiveCommand<Unit, Unit> Redo { get; }
+
+        public ActionsViewModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(IReversibleAction action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
