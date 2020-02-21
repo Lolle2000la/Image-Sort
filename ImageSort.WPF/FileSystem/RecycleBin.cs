5d8a722bc456a5492acf90ef717a3ca5c2f15f54
Luca Auer <lolle2000.la@gmail.com>
2020-02-21 20:51:46 +0100

    Add a recycle bin handler to be implemented on windows

---
+++ ImageSort.WPF/FileSystem/RecycleBin.cs
@@ -0,0 +1,15 @@
+﻿using ImageSort.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSort.WPF.FileSystem
+{
+    class RecycleBin : IRecycleBin
+    {
+        public IDisposable Send(string path, bool confirmationNeeded = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
