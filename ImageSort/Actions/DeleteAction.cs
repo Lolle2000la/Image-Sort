5d9faf485f40e60ab1dd761b45e5c0c657a33e68
Luca Auer <lolle2000.la@gmail.com>
2020-02-21 19:49:50 +0100

    Add a delete action

---
+++ ImageSort/Actions/DeleteAction.cs
@@ -0,0 +1,25 @@
+﻿using ImageSort.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSort.Actions
+{
+    public class DeleteAction : IReversibleAction
+    {
+        public DeleteAction(string path, IRecycleBin fileSystem)
+        {
+
+        }
+
+        public void Act()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Revert()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
