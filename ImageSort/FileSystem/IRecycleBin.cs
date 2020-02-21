fc87d4b55678280552057c4a3f6701306bb2fcda
Luca Auer <lolle2000.la@gmail.com>
2020-02-21 19:49:35 +0100

    Add the concept of a recycle bin

---
+++ ImageSort/FileSystem/IRecycleBin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSort.FileSystem
+{
+    public interface IRecycleBin
+    {
+        /// <summary>
+        /// Sends the file or folder at the given path to the recycle bin.
+        /// </summary>
+        /// <param name="path">The path to the file or folder that should be send to the recycle bin.</param>
+        /// <param name="confirmationNeeded">Should ask for user confirmation before sending to recycle bin.</param>
+        /// <returns>The <see cref="IDisposable"/> will, when disposed, restore the file.</returns>
+        IDisposable Send(string path, bool confirmationNeeded = false);
+    }
+}
