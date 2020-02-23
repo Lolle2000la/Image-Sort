9d2b28fdeac2ab6b2c90e8f7af8506de295b9016
Luca Auer <lolle2000.la@gmail.com>
2020-02-23 22:16:56 +0100

    Implement notification of image deletion/restoration

--- ImageSort/Actions/DeleteAction.cs
+++ ImageSort/Actions/DeleteAction.cs
@@ -1,8 +1,6 @@
 ﻿using ImageSort.FileSystem;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace ImageSort.Actions
 {
@@ -14,13 +12,19 @@ namespace ImageSort.Actions
         private readonly string oldPath;
         private readonly IRecycleBin recycleBin;
         private IDisposable deletedFile;
+        private readonly Action<string> notifyAct;
+        private readonly Action<string> notifyRevert;
 
-        public DeleteAction(string path, IFileSystem fileSystem, IRecycleBin recycleBin)
+        public DeleteAction(string path, IFileSystem fileSystem, IRecycleBin recycleBin,
+            Action<string> notifyAct = null, Action<string> notifyRevert = null)
         {
             if (path == null) throw new ArgumentNullException(nameof(path));
             if (fileSystem == null) throw new ArgumentNullException(nameof(fileSystem));
             if (recycleBin == null) throw new ArgumentNullException(nameof(recycleBin));
             if (!fileSystem.FileExists(path)) throw new FileNotFoundException(null, path);
+
+            this.notifyAct = notifyAct;
+            this.notifyRevert = notifyRevert;
 
             oldPath = path;
             this.recycleBin = recycleBin;
@@ -31,6 +35,8 @@ namespace ImageSort.Actions
         public void Act()
         {
             if (deletedFile == null) deletedFile = recycleBin.Send(oldPath);
+
+            notifyAct?.Invoke(oldPath);
         }
 
         public void Revert()
@@ -38,6 +44,8 @@ namespace ImageSort.Actions
             deletedFile?.Dispose();
 
             deletedFile = null;
+
+            notifyRevert?.Invoke(oldPath);
         }
     }
 }
