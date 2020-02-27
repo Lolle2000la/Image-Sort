4dd1650b13bade4fbc162d8958f4e82bf28a5e57
Luca Auer <lolle2000.la@gmail.com>
2020-02-27 22:17:26 +0100

    Fix the automatic updater

--- ImageSort.WindowsUpdater/InstallerRunner.cs
+++ ImageSort.WindowsUpdater/InstallerRunner.cs
@@ -18,11 +18,11 @@ namespace ImageSort.WindowsUpdater
 
             if (!Directory.Exists(tempFolder)) Directory.CreateDirectory(tempFolder);
 
-            var fs = File.Create(Path.Combine(tempFolder, "imagesort.exe"));
+            var fs = File.Create(Path.Combine(tempFolder, setupPath));
 
             await installer.CopyToAsync(fs);
 
-            fs.Close();
+            await fs.DisposeAsync();
 
             RunSetup(setupPath);
 
@@ -31,7 +31,13 @@ namespace ImageSort.WindowsUpdater
 
         private static void RunSetup(string path)
         {
-            Process.Start("cmd", $"/c @ping -n 5 localhost> nul & msiexec /i \"{path}\" TARGETDIR=\"{AppDomain.CurrentDomain.BaseDirectory}\" /passive");
+            var processStartInfo = new ProcessStartInfo("msiexec", $"/i \"{path}\" TARGETDIR=\"{AppDomain.CurrentDomain.BaseDirectory}\" /passive")
+            {
+                Verb = "runas",
+                UseShellExecute = true
+            };
+
+            Process.Start(processStartInfo);
         }
     }
 }
