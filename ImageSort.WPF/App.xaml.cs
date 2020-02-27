1ec9d7a37dde926081e787fc3fc2ee4df5182509
Luca Auer <lolle2000.la@gmail.com>
2020-02-27 20:37:26 +0100

    Add updater to startup

--- ImageSort.WPF/App.xaml.cs
+++ ImageSort.WPF/App.xaml.cs
@@ -1,11 +1,15 @@
 ﻿using ImageSort.DependencyManagement;
 using ImageSort.FileSystem;
+using ImageSort.WindowsUpdater;
 using ImageSort.WPF.FileSystem;
+using Octokit;
 using ReactiveUI;
 using Splat;
 using System;
 using System.Reflection;
+using System.Threading.Tasks;
 using System.Windows;
+using Application = System.Windows.Application;
 
 namespace ImageSort.WPF
 {
@@ -16,11 +20,36 @@ namespace ImageSort.WPF
     {
         public App()
         {
+            Startup += OnStartup;
+
             Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
             Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetEntryAssembly());
             Locator.CurrentMutable.RegisterManditoryDependencies();
             Locator.CurrentMutable.Register<IRecycleBin>(() => new RecycleBin());
+        }
+
+        private async void OnStartup(object sender, StartupEventArgs e)
+        {
+            var ghubClient = new GitHubClient(new ProductHeaderValue("Image-Sort"));
+            var updateFetcher = new GitHubUpdateFetcher(ghubClient);
+            (var success, var release) = await updateFetcher.TryGetLatestReleaseAsync(true);
+
+            if (success && MessageBox.Show($"A new version of Image Sort is available: {release.TagName} \n\nDo you want to update?", 
+                    "An update is available", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                if (updateFetcher.TryGetInstallerFromRelease(release, out var installerAsset))
+                {
+                    var installer = await updateFetcher.GetStreamFromAssetAsync(installerAsset);
+
+                    InstallerRunner.RunAsync(installer);
+                }
+            }
+        }
+
+        ~App()
+        {
+            Startup -= OnStartup;
         }
     }
 }
