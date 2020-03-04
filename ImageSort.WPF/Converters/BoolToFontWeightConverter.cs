7673ef2e709fd7e268574e13a6b0a77331598589
Luca Auer <lolle2000.la@gmail.com>
2020-03-04 21:31:27 +0100

    Add BoolToFontWeightConverter

---
+++ ImageSort.WPF/Converters/BoolToFontWeightConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace ImageSort.WPF.Converters
+{
+    class BoolToFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? FontWeights.Bold : FontWeights.Normal;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
