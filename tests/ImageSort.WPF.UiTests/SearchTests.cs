0598d9f48b12d099b32013162daa0ab6bb31edb4
Luca Auer <lolle2000.la@gmail.com>
2020-03-08 17:11:27 +0100

    Setup test

---
+++ tests/ImageSort.WPF.UiTests/SearchTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlaUI.Core;
+using FlaUI.UIA3;
+
+namespace ImageSort.WPF.UiTests
+{
+    public class SearchTests
+    {
+        private readonly Application app;
+        private readonly UIA3Automation automation;
+
+        public SearchTests()
+        {
+            app = Application.Launch("Image Sort.exe");
+            automation = new UIA3Automation();
+        }
+
+
+
+        ~SearchTests()
+        {
+            automation.Dispose();
+            app.Dispose();
+        }
+    }
+}
