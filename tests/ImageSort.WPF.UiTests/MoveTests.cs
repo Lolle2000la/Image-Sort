c31ab7aa214f88391f16bb9e1722d6f29c58ca25
Luca Auer <lolle2000.la@gmail.com>
2020-03-08 20:04:15 +0100

    Setup Move tests skeleton

---
+++ tests/ImageSort.WPF.UiTests/MoveTests.cs
@@ -0,0 +1,28 @@
+﻿using FlaUI.UIA3;
+using FlaUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FlaUI.Core.AutomationElements;
+
+namespace ImageSort.WPF.UiTests
+{
+    public class MoveTests : IDisposable
+    {
+        private readonly Application app;
+        private readonly UIA3Automation automation;
+        private readonly string currentPath;
+        private readonly Window mainWindow;
+
+        public MoveTests()
+        {
+            (currentPath, app, automation, mainWindow) = SetupTeardownHelper.Setup();
+        }
+
+        public void Dispose()
+        {
+            SetupTeardownHelper.TearDown(currentPath, app, automation);
+        }
+    }
+}
