3b726b3993e19490c6f3115170d5a27babfb3fee
Luca Auer <lolle2000.la@gmail.com>
2020-03-08 18:51:02 +0100

    Add first ui test

--- tests/ImageSort.WPF.UiTests/SearchTests.cs
+++ tests/ImageSort.WPF.UiTests/SearchTests.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
 using FlaUI.UIA3;
 using Xunit;
 
@@ -14,15 +16,27 @@ namespace ImageSort.WPF.UiTests
         private readonly Application app;
         private readonly UIA3Automation automation;
         private readonly string currentPath;
+        private readonly Window mainWindow;
 
         public SearchTests()
         {
-            (currentPath, app, automation) = SetupTeardownHelper.Setup();
+            (currentPath, app, automation, mainWindow) = SetupTeardownHelper.Setup();
         }
 
         [Fact(DisplayName = "Filters out images correctly")]
         public void FiltersOutImagesCorrectly()
         {
+            var search = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("SearchTerm")).AsTextBox();
+            search.Focus();
+            search.Text = ".jpg";
+
+            var images = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("Images")).AsListBox()
+                .FindAllChildren()
+                .Select(e => e.AsListBoxItem())
+                .Select(e => e.Name)
+                .Select(n => Path.GetFileName(n));
+
+            Assert.DoesNotContain("mock 4.png", images);
         }
 
         public void Dispose()
