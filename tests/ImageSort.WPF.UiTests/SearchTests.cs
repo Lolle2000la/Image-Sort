bcafb98271381bc06aa10c183ab6e4b27bf17f85
Luca Auer <lolle2000.la@gmail.com>
2020-03-08 17:26:40 +0100

    Make sure test works on mock directory

--- tests/ImageSort.WPF.UiTests/SearchTests.cs
+++ tests/ImageSort.WPF.UiTests/SearchTests.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 using FlaUI.Core;
 using FlaUI.UIA3;
+using Xunit;
 
 namespace ImageSort.WPF.UiTests
 {
@@ -10,19 +13,50 @@ namespace ImageSort.WPF.UiTests
     {
         private readonly Application app;
         private readonly UIA3Automation automation;
+        private readonly string currentPath;
 
         public SearchTests()
         {
-            app = Application.Launch("Image Sort.exe");
+            currentPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Guid.NewGuid().ToString());
+
+            CopyFolder(Path.GetFullPath("MockState"), currentPath);
+
+            app = Application.Launch(new ProcessStartInfo("Image Sort.exe", $"\"{currentPath}\""));
             automation = new UIA3Automation();
         }
 
-
+        [Fact(DisplayName = "Filters out images correctly")]
+        public void FiltersOutImagesCorrectly()
+        {
+        }
 
         ~SearchTests()
         {
             automation.Dispose();
             app.Dispose();
+        }
+
+        private static void CopyFolder(string sourceFolder, string destFolder)
+        {
+            if (!Directory.Exists(destFolder)) Directory.CreateDirectory(destFolder);
+
+            var files = Directory.GetFiles(sourceFolder);
+
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string dest = Path.Combine(destFolder, name);
+                File.Copy(file, dest);
+            }
+
+            var folders = Directory.GetDirectories(sourceFolder);
+
+            foreach (string folder in folders)
+            {
+                string name = Path.GetFileName(folder);
+                string dest = Path.Combine(destFolder, name);
+                CopyFolder(folder, dest);
+            }
         }
     }
 }
