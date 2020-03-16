45cd673c907e8103bef21088b60dbbc1724dc607
Luca Auer <lolle2000.la@gmail.com>
2020-03-17 00:41:09 +0100

    Add shortcut for moving

--- src/ImageSort.WPF/SettingsManagement/ShortCutManagement/KeybindingsSettingsGroupViewModel.cs
+++ src/ImageSort.WPF/SettingsManagement/ShortCutManagement/KeybindingsSettingsGroupViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using ImageSort.Localization;
 using ImageSort.SettingsManagement;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace ImageSort.WPF.SettingsManagement.ShortCutManagement
 {
@@ -11,5 +13,13 @@ namespace ImageSort.WPF.SettingsManagement.ShortCutManagement
         public override string Name => "KeyBindings";
 
         public override string Header => Text.KeyBindingsSettingsHeader;
+
+        private Key _move = Key.Up;
+
+        public Key Move
+        {
+            get => _move;
+            set => this.RaiseAndSetIfChanged(ref _move, value);
+        }
     }
 }
