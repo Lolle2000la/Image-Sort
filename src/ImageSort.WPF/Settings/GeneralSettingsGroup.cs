82cbc0376fc62da0909da3a95b1ceeb196a24431
Luca Auer <lolle2000.la@gmail.com>
2020-03-16 19:08:51 +0100

    Add general settings group

---
+++ src/ImageSort.WPF/Settings/GeneralSettingsGroup.cs
@@ -0,0 +1,43 @@
+﻿using ImageSort.SettingsManagement;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSort.WPF.Settings
+{
+    class GeneralSettingsGroup : SettingsGroupViewModelBase
+    {
+        public override string Name => "General";
+
+        public override string Header => "General";
+
+        private bool _darkMode = false;
+
+        public bool DarkMode
+        {
+            get => _darkMode;
+            set => this.RaiseAndSetIfChanged(ref _darkMode, value);
+        }
+
+        private bool _checkForUpdatesOnStartup = false;
+
+        public bool CheckForUpdatesOnStartup
+        {
+            get => _checkForUpdatesOnStartup;
+            set => this.RaiseAndSetIfChanged(ref _checkForUpdatesOnStartup, value);
+        }
+
+        private bool _installPrereleaseBuilds = false;
+
+        public bool InstallPrereleaseBuilds
+        {
+            get => _installPrereleaseBuilds;
+            set => this.RaiseAndSetIfChanged(ref _installPrereleaseBuilds, value);
+        }
+
+        public GeneralSettingsGroup() : base()
+        {
+        }
+    }
+}
