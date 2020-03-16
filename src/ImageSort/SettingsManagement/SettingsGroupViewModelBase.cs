7d7151543804b84e0e0c7582623259fc1c9dc67c
Luca Auer <lolle2000.la@gmail.com>
2020-03-16 17:45:53 +0100

    Add name property for storage

--- src/ImageSort/SettingsManagement/SettingsGroupViewModelBase.cs
+++ src/ImageSort/SettingsManagement/SettingsGroupViewModelBase.cs
@@ -6,6 +6,10 @@ namespace ImageSort.SettingsManagement
 {
     public abstract class SettingsGroupViewModelBase : ReactiveObject
     {
+        /// <summary>
+        /// Used for storage. Should not be changed EVER once set. It must also be unique.
+        /// </summary>
+        public abstract string Name { get; }
         public abstract string Header { get; }
         protected Dictionary<string, object> SettingsStore { get; } = new Dictionary<string, object>();
         public IReadOnlyDictionary<string, object> SettingsModel => new ReadOnlyDictionary<string, object>(SettingsStore);
