a88136fe838541b3f8d3a0ea885876ddbbf36d87
Luca Auer <lolle2000.la@gmail.com>
2020-03-16 20:04:49 +0100

    Bind general settings properties to view

--- src/ImageSort.WPF/Views/GeneralSettingsGroupView.xaml.cs
+++ src/ImageSort.WPF/Views/GeneralSettingsGroupView.xaml.cs
@@ -1,7 +1,9 @@
-﻿using ImageSort.WPF.SettingsManagement;
+﻿using AdonisUI;
+using ImageSort.WPF.SettingsManagement;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,36 @@ namespace ImageSort.WPF.Views
         public GeneralSettingsGroupView()
         {
             InitializeComponent();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+                this.Bind(ViewModel,
+                    vm => vm.DarkMode,
+                    view => view.DarkMode.IsChecked)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel,
+                    vm => vm.CheckForUpdatesOnStartup,
+                    view => view.CheckForUpdates.IsChecked)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel,
+                    vm => vm.InstallPrereleaseBuilds,
+                    view => view.InstallPrereleaseBuilds.IsChecked)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel,
+                    vm => vm.CheckForUpdatesOnStartup,
+                    view => view.InstallPrereleaseBuilds.IsEnabled)
+                    .DisposeWith(disposableRegistration);
+
+                ViewModel.WhenAnyValue(vm => vm.DarkMode)
+                    .Subscribe(SetDarkMode);
+            });
+        }
+        private void SetDarkMode(bool darkMode)
+        {
+            ResourceLocator.SetColorScheme(Application.Current.Resources, darkMode ? ResourceLocator.DarkColorScheme : ResourceLocator.LightColorScheme);
         }
     }
 }
