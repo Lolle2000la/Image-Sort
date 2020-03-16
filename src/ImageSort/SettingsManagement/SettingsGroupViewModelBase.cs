7fd7a92a97f461fa5ab0df3054cfd064ec8fec62
Luca Auer <lolle2000.la@gmail.com>
2020-03-16 17:12:00 +0100

    Outline a base for settings groups

--- src/ImageSort/SettingsManagement/SettingsGroupViewModelBase.cs
+++ src/ImageSort/SettingsManagement/SettingsGroupViewModelBase.cs
@@ -1,8 +1,13 @@
 ﻿using ReactiveUI;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace ImageSort.SettingsManagement
 {
-    public class SettingsGroupViewModelBase : ReactiveObject
+    public abstract class SettingsGroupViewModelBase : ReactiveObject
     {
+        public abstract string Header { get; }
+        protected Dictionary<string, object> SettingsStore { get; } = new Dictionary<string, object>();
+        public IReadOnlyDictionary<string, object> SettingsModel => new ReadOnlyDictionary<string, object>(SettingsStore);
     }
 }
