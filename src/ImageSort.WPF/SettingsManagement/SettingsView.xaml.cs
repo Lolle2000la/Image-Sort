0371ec1e371fc7a8fdfb1d2908de665fa516988d
Luca Auer <lolle2000.la@gmail.com>
2020-03-16 20:11:25 +0100

    Add view for SettingsViewModel

---
+++ src/ImageSort.WPF/SettingsManagement/SettingsView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using AdonisUI.Controls;
+using ImageSort.SettingsManagement;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImageSort.WPF.SettingsManagement
+{
+    /// <summary>
+    /// Interaction logic for SettingsView.xaml
+    /// </summary>
+    public partial class SettingsView : AdonisWindow, IViewFor<SettingsViewModel>
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+
+        #region IViewFor implementation
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty
+            .Register(nameof(ViewModel), typeof(SettingsViewModel), typeof(SettingsView), new PropertyMetadata(null));
+
+        public SettingsViewModel ViewModel
+        {
+            get => (SettingsViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (SettingsViewModel)value;
+        }
+
+        #endregion IViewFor implementation
+    }
+}
