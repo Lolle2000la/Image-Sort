0af93eed282bdb41101bedb79a30ca593fbd16da
Luca Auer <lolle2000.la@gmail.com>
2020-03-16 19:20:29 +0100

    Fix name collusion

--- src/ImageSort.WPF/SettingsManagement/GeneralSettingsGroup.cs
+++ src/ImageSort.WPF/SettingsManagement/GeneralSettingsGroup.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ImageSort.WPF.Settings
+namespace ImageSort.WPF.SettingsManagement
 {
     class GeneralSettingsGroup : SettingsGroupViewModelBase
     {
