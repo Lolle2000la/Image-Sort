a5cae7d542282c077fd1f3541317619d1e2fab09
Luca Auer <lolle2000.la@gmail.com>
2020-03-16 21:05:30 +0100

    Add helper methods for saving and restoring settings

---
+++ src/ImageSort.WPF/SettingsManagement/SettingsHelper.cs
@@ -0,0 +1,21 @@
+﻿using ImageSort.SettingsManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSort.WPF.SettingsManagement
+{
+    internal static class SettingsHelper
+    {
+        public static void Save(this SettingsViewModel settings)
+        {
+            Settings.Default["Settings"] = settings.AsDictionary();
+            Settings.Default.Save();
+        }
+
+        public static void Restore(this SettingsViewModel settings)
+        {
+            settings.RestoreFromDictionary((Dictionary<string, Dictionary<string, object>>)Settings.Default["Settings"]);
+        }
+    }
+}
