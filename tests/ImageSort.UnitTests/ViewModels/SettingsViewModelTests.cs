af6ed4b342f88651cb25db8753de6a69c85c699d
Luca Auer <lolle2000.la@gmail.com>
2020-03-16 17:51:02 +0100

    Add test verifying dictionary conversion behavior

--- tests/ImageSort.UnitTests/ViewModels/SettingsViewModelTests.cs
+++ tests/ImageSort.UnitTests/ViewModels/SettingsViewModelTests.cs
@@ -11,21 +11,23 @@ namespace ImageSort.UnitTests.ViewModels
     {
         public class FirstGroupMock : SettingsGroupViewModelBase
         {
+            public override string Name => "FirstGroup";
             public override string Header => "First group";
 
             public FirstGroupMock()
             {
-                SettingsStore["some_setting"] = "fake value";
+                SettingsStore["some_setting"] = "fake value 1";
             }
         }
 
         public class SecondGroupMock : SettingsGroupViewModelBase
         {
+            public override string Name => "SecondGroup";
             public override string Header => "Second group";
 
             public SecondGroupMock()
             {
-                SettingsStore["some_setting"] = "fake value";
+                SettingsStore["some_setting"] = "fake value 2";
             }
         }
 
@@ -45,6 +47,27 @@ namespace ImageSort.UnitTests.ViewModels
 
             Assert.Equal(firstGroup, settingsVM.GetGroup<FirstGroupMock>());
             Assert.Equal(secondGroup, settingsVM.GetGroup<SecondGroupMock>());
+        }
+
+        [Fact(DisplayName = "Correctly converts settings into a dictionary")]
+        public void CorrectlyConvertsToDictionary()
+        {
+            var firstGroup = new FirstGroupMock();
+            var secondGroup = new SecondGroupMock();
+
+            var settingsGroups = new SettingsGroupViewModelBase[]
+            {
+                firstGroup,
+                secondGroup
+            };
+
+            var settingsVM = new SettingsViewModel(settingsGroups);
+
+            var settingsDict = settingsVM.AsDictionary();
+
+            Assert.Equal("fake value 2", settingsDict[secondGroup.Name]["some_setting"]);
+
+            Assert.Equal("fake value 1", settingsDict[firstGroup.Name]["some_setting"]);
         }
     }
 }
