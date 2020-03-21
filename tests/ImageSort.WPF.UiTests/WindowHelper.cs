caebb8e66aada7a64700a3cf19a91ee962810425
Luca Auer <lolle2000.la@gmail.com>
2020-03-21 13:31:50 +0100

    Simplify fetching the selected image

--- tests/ImageSort.WPF.UiTests/WindowHelper.cs
+++ tests/ImageSort.WPF.UiTests/WindowHelper.cs
@@ -8,10 +8,17 @@ namespace ImageSort.WPF.UiTests
 {
     static class WindowHelper
     {
+        private static ListBox GetImagesBox(this Window mainWindow) => mainWindow
+            .FindFirstDescendant(cf => cf.ByAutomationId("Images")).FindFirstDescendant(cf => cf.ByAutomationId("Images")).AsListBox();
+
         public static IEnumerable<string> GetImages(this Window mainWindow) => mainWindow
-            .FindFirstDescendant(cf => cf.ByAutomationId("Images")).FindFirstDescendant(cf => cf.ByAutomationId("Images")).AsListBox()
+            .GetImagesBox()
             .FindAllChildren()
             .Select(e => e.AsListBoxItem())
             .Select(e => e.Name);
+
+        public static string GetSelectedImage(this Window mainWindow) => mainWindow
+            .GetImagesBox()
+            .SelectedItem.Name;
     }
 }
