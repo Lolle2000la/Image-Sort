fdf075bd4fe543a4ebaaaa878e988c20dcb8fb17
Luca Auer <lolle2000.la@gmail.com>
2020-03-25 16:16:58 +0100

    Add collection for testing folder actions

---
+++ tests/ImageSort.WPF.UiTests/FolderActionsTests.cs
@@ -0,0 +1,24 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ImageSort.WPF.UiTests
+{
+    [Collection("App collection")]
+    public class FolderActionsTests
+    {
+        private readonly Application app;
+        private readonly UIA3Automation automation;
+        private readonly string currentPath;
+        private readonly Window mainWindow;
+
+        public FolderActionsTests(AppFixture appFixture)
+        {
+            (currentPath, app, automation, mainWindow) = appFixture;
+        }
+    }
+}
