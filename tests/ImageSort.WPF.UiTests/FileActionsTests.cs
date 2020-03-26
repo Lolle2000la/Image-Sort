9d3971eb291086e8d8ad4b253a03cb70121dad71
Luca Auer <lolle2000.la@gmail.com>
2020-03-26 17:43:10 +0100

    Try unskip test

--- tests/ImageSort.WPF.UiTests/FileActionsTests.cs
+++ tests/ImageSort.WPF.UiTests/FileActionsTests.cs
@@ -106,7 +106,7 @@ namespace ImageSort.WPF.UiTests
             Assert.True(File.Exists(file));
         }
 
-        [Fact(DisplayName = "Can rename images", Skip = "Broken")]
+        [Fact(DisplayName = "Can rename images")]
         public void CanRenameImages()
         {
             var file = mainWindow.GetSelectedImage();
