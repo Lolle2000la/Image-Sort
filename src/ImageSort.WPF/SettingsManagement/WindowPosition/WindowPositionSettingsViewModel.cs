974c9d514fb51438fa1f0434298131d20b0aaa90
Luca Auer <lolle2000.la@gmail.com>
2020-04-08 16:22:16 +0200

    Add settings storing the state of the window

---
+++ src/ImageSort.WPF/SettingsManagement/WindowPosition/WindowPositionSettingsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ImageSort.SettingsManagement;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageSort.WPF.SettingsManagement.WindowPosition
+{
+    public class WindowPositionSettingsViewModel : SettingsGroupViewModelBase
+    {
+        public override string Name => "WindowPosition";
+
+        public override string Header => "Window position";
+
+        public override bool IsVisible => false;
+
+        private bool _isMaximized = false;
+        public bool IsMaximized
+        {
+            get => _isMaximized;
+            set => this.RaiseAndSetIfChanged(ref _isMaximized, value);
+        }
+
+        // position
+        private int _left = 100;
+        public int Left
+        {
+            get => _left;
+            set => this.RaiseAndSetIfChanged(ref _left, value);
+        }
+
+        private int _top = 100;
+        public int Top
+        {
+            get => _top;
+            set => this.RaiseAndSetIfChanged(ref _top, value);
+        }
+
+        // size
+        private int _width = 100;
+        public int Width
+        {
+            get => _width;
+            set => this.RaiseAndSetIfChanged(ref _width, value);
+        }
+
+        private int _height = 100;
+        public int Height
+        {
+            get => _height;
+            set => this.RaiseAndSetIfChanged(ref _height, value);
+        }
+    }
+}
