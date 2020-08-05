2620cb4d305ac494944a54f98140b02a00352255
Luca Auer <lolle2000.la@gmail.com>
2020-08-05 13:00:41 +0200

    Add empty credits window

---
+++ src/ImageSort.WPF/Views/Credits/CreditsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AdonisUI.Controls;
+
+namespace ImageSort.WPF.Views.Credits
+{
+    /// <summary>
+    /// Interaction logic for CreditsWindow.xaml
+    /// </summary>
+    public partial class CreditsWindow : AdonisWindow
+    {
+        public CreditsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
