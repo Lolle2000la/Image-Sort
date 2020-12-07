97f506607a4ff7bb4d1211b19d4f1e4197e34cc7
Luca Auer <lolle2000.la@gmail.com>
2020-12-07 09:40:23 +0100

    Create factory for FolderViewModels

---
+++ src/ImageSort/ViewModels/FolderViewModelFactory.cs
@@ -0,0 +1,33 @@
+﻿using ImageSort.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSort.ViewModels
+{
+    public class FolderViewModelFactory
+    {
+        private readonly IFileSystem fileSystem;
+        private readonly Func<FileSystemWatcher> folderWatcherFactory;
+        private readonly IScheduler backgroundScheduler;
+
+        public FolderViewModelFactory(IFileSystem fileSystem, Func<FileSystemWatcher> folderWatcherFactory, IScheduler backgroundScheduler)
+        {
+            this.fileSystem = fileSystem;
+            this.folderWatcherFactory = folderWatcherFactory;
+            this.backgroundScheduler = backgroundScheduler;
+        }
+
+        public FolderViewModel GetFor(string path)
+        {
+            return new FolderViewModel(fileSystem, folderWatcherFactory, backgroundScheduler)
+            {
+                Path = path
+            };
+        }
+    }
+}
