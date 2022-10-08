d2b9a152da27b6ac7ef4ebcaed36d53af6844d96
Luca Auer <lolle2000.la@gmail.com>
2022-10-09 00:50:51 +0200

    Allow disabling of gifs in thumbnails

--- src/ImageSort.WPF/Converters/PathToBitmapImageConverter.cs
+++ src/ImageSort.WPF/Converters/PathToBitmapImageConverter.cs
@@ -1,7 +1,12 @@
-﻿using ImageSort.WPF.FileSystem;
+﻿using ImageSort.SettingsManagement;
+using ImageSort.WPF.FileSystem;
+using ImageSort.WPF.SettingsManagement;
+using Splat;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using System.Linq;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
@@ -11,12 +16,18 @@ namespace ImageSort.WPF.Converters
     internal class PathToBitmapImageConverter : IValueConverter
     {
         public int? LoadWidth { get; set; } = null;
+        public bool ForGifThumbnails { get; set; } = false;
+        
+        private GeneralSettingsGroupViewModel generalSettings = Locator.Current.GetService<IEnumerable<SettingsGroupViewModelBase>>()
+                .Select(s => s as GeneralSettingsGroupViewModel)
+                .First(s => s != null);
 
         [SuppressMessage("Design", "CA1031:Do not catch general exception types",
             Justification = "The app should not crash just because some exception happened")]
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null) return null;
+            if (ForGifThumbnails && (!generalSettings.AnimateGifThumbnails || !generalSettings.AnimateGifs)) return null; // prevent gifs from loading when disabled.
 
             if (value is string path)
                 return ImageLoading.GetImageFromPath(path);
