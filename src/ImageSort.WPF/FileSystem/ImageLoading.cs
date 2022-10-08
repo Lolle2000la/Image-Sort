b6856e4b133ba86a7f7f7928bd0371e632ac5d71
Luca Auer <lolle2000.la@gmail.com>
2022-10-08 02:09:09 +0200

    Add caching via LazyCache

--- src/ImageSort.WPF/FileSystem/ImageLoading.cs
+++ src/ImageSort.WPF/FileSystem/ImageLoading.cs
@@ -1,5 +1,9 @@
 ﻿using ImageSort.Localization;
+using LazyCache;
+using LazyCache.Providers;
+using Microsoft.Extensions.Caching.Memory;
 using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Windows;
@@ -10,29 +14,44 @@ namespace ImageSort.WPF.FileSystem
 {
     internal static class ImageLoading
     {
+        static IAppCache cache = new CachingService(
+            new MemoryCacheProvider(
+                new MemoryCache(
+                    new MemoryCacheOptions()
+                    {
+                        SizeLimit = 20, // limit the maximum number of 
+                    })));
+
+        static MemoryCacheEntryOptions options = new MemoryCacheEntryOptions()
+        {
+            Size = 1, // the same unit must be used, as MemoryCache itself knows no units. Here, 1 equals 1 element.
+        };
+
         public static ImageSource GetImageFromPath(string path)
         {
             if (path == null) return null;
 
-            try
+            return cache.GetOrAdd<ImageSource>(path, () =>
             {
-                var bitmapImage = new BitmapImage();
-
-                bitmapImage.BeginInit();
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.UriSource = new Uri(path);
-                bitmapImage.EndInit();
-
-                return bitmapImage;
-            }
-            catch (Exception ex)
-            {
-                var textDrawing = new GeometryDrawing
+                try
                 {
-                    Geometry = new GeometryGroup
+                    var bitmapImage = new BitmapImage();
+
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.UriSource = new Uri(path);
+                    bitmapImage.EndInit();
+
+                    return bitmapImage;
+                }
+                catch (Exception ex)
+                {
+                    var textDrawing = new GeometryDrawing
                     {
-                        Children = new GeometryCollection(new[]
+                        Geometry = new GeometryGroup
                         {
+                            Children = new GeometryCollection(new[]
+                            {
                             new FormattedText(Text.CouldNotLoadImageErrorText
                                         .Replace("{ErrorMessage}", ex.Message, StringComparison.OrdinalIgnoreCase)
                                         .Replace("{FileName}", Path.GetFileName(path),
@@ -44,13 +63,14 @@ namespace ImageSort.WPF.FileSystem
                                     Brushes.Black, 1)
                                 .BuildGeometry(new Point(8, 8))
                         })
-                    },
-                    Brush = Brushes.Black,
-                    Pen = new Pen(Brushes.White, 0.5)
-                };
+                        },
+                        Brush = Brushes.Black,
+                        Pen = new Pen(Brushes.White, 0.5)
+                    };
 
-                return new DrawingImage(textDrawing);
-            }
+                    return new DrawingImage(textDrawing);
+                }
+            }, options);
         }
     }
 }
