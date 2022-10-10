891c2500b71fb67679ba759e082fde9d278da7ad
Luca Auer <lolle2000.la@gmail.com>
2022-10-10 22:39:04 +0200

    Add view for metadata

---
+++ src/ImageSort.WPF/Views/Metadata/MetadataView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ImageSort.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageSort.WPF.Views.Metadata
+{
+    /// <summary>
+    /// Interaction logic for MetadataView.xaml
+    /// </summary>
+    public partial class MetadataView : ReactiveUserControl<MetadataViewModel>
+    {
+        public MetadataView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(disposableRegistration =>
+            {
+            });
+        }
+    }
+}
