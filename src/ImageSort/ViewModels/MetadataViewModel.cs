c73eab008a7369b35e303468785ef5e9ba328df0
Luca Auer <lolle2000.la@gmail.com>
2022-10-10 17:02:09 +0200

    Initial modelling

---
+++ src/ImageSort/ViewModels/MetadataViewModel.cs
@@ -0,0 +1,38 @@
+﻿using ImageSort.FileSystem;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSort.ViewModels
+{
+    public class MetadataViewModel : ReactiveObject
+    {
+        private readonly IMetadataExtractor extractor;
+        private readonly IFileSystem fileSystem;
+
+        private string _imagePath;
+        public string ImagePath
+        {
+            get => _imagePath;
+            set => this.RaiseAndSetIfChanged(ref _imagePath, value);
+        }
+
+        private ObservableAsPropertyHelper<Dictionary<string, Dictionary<string, string>>> _metadata;
+        public Dictionary<string, Dictionary<string, string>> Metadata => _metadata.Value;
+
+        public MetadataViewModel(IMetadataExtractor extractor, IFileSystem fileSystem)
+        {
+            this.extractor = extractor;
+            this.fileSystem = fileSystem;
+
+            _metadata = this.WhenAnyValue(x => x.ImagePath)
+                .Where(x => fileSystem.FileExists(x))
+                .Select(x => extractor.Extract(x))
+                .ToProperty(this, x => x.Metadata);
+        }
+    }
+}
