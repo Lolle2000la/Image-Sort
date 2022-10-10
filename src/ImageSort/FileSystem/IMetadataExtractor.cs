ac4f100656fc0df228de290af7659356361b32ef
Luca Auer <lolle2000.la@gmail.com>
2022-10-10 17:02:09 +0200

    Initial modelling

---
+++ src/ImageSort/FileSystem/IMetadataExtractor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ImageSort.FileSystem
+{
+    public interface IMetadataExtractor
+    {
+        Dictionary<string, Dictionary<string, string>> Extract(string x);
+    }
+}
