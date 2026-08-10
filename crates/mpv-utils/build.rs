73c134917048ec134e5a2407ecd833573b7dc752
Luca Auer <lolle2000.la@gmail.com>
2026-08-10 14:30:46 +0200

    chore(build): rename winbuild feature to vendor-windows

--- crates/mpv-utils/build.rs
+++ crates/mpv-utils/build.rs
@@ -1,12 +1,12 @@
 fn main() {
-    #[cfg(all(target_os = "windows", feature = "winbuild"))]
+    #[cfg(all(target_os = "windows", feature = "vendor-windows"))]
     setup_mpv_windows();
 }
 
 #[allow(dead_code)]
 const DEFAULT_MPV_WINBUILD_TAG: &str = "2026-08-08-dd5d17d328";
 
-#[cfg(all(target_os = "windows", feature = "winbuild"))]
+#[cfg(all(target_os = "windows", feature = "vendor-windows"))]
 fn setup_mpv_windows() {
     use std::env;
     use std::fs;
